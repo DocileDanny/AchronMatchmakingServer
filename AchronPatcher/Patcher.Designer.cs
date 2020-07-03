32cad0e65e0351084858f9b4e372e8d50b726f5e
Danny <dannylisher@lycos.com>
2020-07-03 11:43:48 +0100

    Added a patcher

---
+++ AchronPatcher/Patcher.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace AchronPatcher
+{
+    partial class Patcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPatch = new System.Windows.Forms.Button();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.lblTarget = new System.Windows.Forms.Label();
+            this.ofdClient = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnPatch
+            // 
+            this.btnPatch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPatch.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnPatch.Location = new System.Drawing.Point(13, 71);
+            this.btnPatch.Name = "btnPatch";
+            this.btnPatch.Size = new System.Drawing.Size(212, 62);
+            this.btnPatch.TabIndex = 0;
+            this.btnPatch.Text = "Patch Client";
+            this.btnPatch.UseVisualStyleBackColor = true;
+            this.btnPatch.Click += new System.EventHandler(this.btnPatch_Click);
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtTarget.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtTarget.Location = new System.Drawing.Point(13, 34);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(212, 22);
+            this.txtTarget.TabIndex = 1;
+            this.txtTarget.Text = "127.0.0.1";
+            // 
+            // lblTarget
+            // 
+            this.lblTarget.AutoSize = true;
+            this.lblTarget.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblTarget.Location = new System.Drawing.Point(12, 7);
+            this.lblTarget.Name = "lblTarget";
+            this.lblTarget.Size = new System.Drawing.Size(106, 17);
+            this.lblTarget.TabIndex = 2;
+            this.lblTarget.Text = "Target Address";
+            // 
+            // Patcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(239, 151);
+            this.Controls.Add(this.lblTarget);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.btnPatch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Patcher";
+            this.Text = "Achron Patcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPatch;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Label lblTarget;
+        private System.Windows.Forms.OpenFileDialog ofdClient;
+    }
+}
+
---
+++ AchronPatcher/Patcher.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AchronPatcher
+{
+    public partial class Patcher : Form
+    {
+        public Patcher()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPatch_Click(object sender, EventArgs e)
+        {
+            //A file was selected.
+            if (ofdClient.ShowDialog() == DialogResult.OK)
+            {
+                //could be correct..
+                if (ofdClient.FileName.ToLower().Contains(".exe") && ofdClient.FileName.ToLower().Contains("achron"))
+                {
+                    string name = ofdClient.FileName + ".backup-" + DateTime.Now.Ticks;
+                    System.IO.File.Move(ofdClient.FileName, name);
+
+                    System.IO.Stream clientStream = System.IO.File.Open(name, System.IO.FileMode.Open);
+                    byte[] data = ToByteArray(clientStream);
+
+                    byte[] target = new byte[] { 0x77, 0x77, 0x77, 0x2E,
+                                                 0x61, 0x63, 0x68, 0x72,
+                                                 0x6F, 0x6E, 0x67, 0x61,
+                                                 0x6D, 0x65, 0x2E, 0x63,
+                                                 0x6F, 0x6D };
+
+                    byte[] result = new byte[] { 0x00, 0x00, 0x00, 0x00,
+                                                 0x00, 0x00, 0x00, 0x00,
+                                                 0x00, 0x00, 0x00, 0x00,
+                                                 0x00, 0x00, 0x00, 0x00,
+                                                 0x00, 0x00 };
+
+                    byte[] tResult = ASCIIEncoding.ASCII.GetBytes(txtTarget.Text);
+
+                    if (tResult.Length > target.Length)
+                    {
+                        MessageBox.Show("Target Address Too Long!");
+                        return;
+                    }
+
+                    tResult.CopyTo(result, 0);
+
+                    int found = -1;
+                    int replaced = 0;
+                    do
+                    {
+                        found = SearchBytes(data, target);
+
+                        if (found != -1)
+                        {
+                            //replace the dest with the target
+                            for (int x = 0; x < target.GetUpperBound(0) + 1; x++)
+                            {
+                                data[found + x] = result[x];
+                            }
+                            replaced++;
+                        }
+
+                    } while (found != -1);
+
+                    if (replaced != 0)
+                    {
+                        System.IO.File.WriteAllBytes(ofdClient.FileName, data);
+                        MessageBox.Show("Client patched! (" + replaced + ")");
+                    }
+                    else
+                    {
+                        System.IO.File.WriteAllBytes(ofdClient.FileName, data);
+                        MessageBox.Show("Client not patched! (Already patched? Wrong file?)");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select Achron.exe!");
+                }
+
+            }
+        }
+
+
+        static int SearchBytes(byte[] haystack, byte[] needle)
+        {
+            var len = needle.Length;
+            var limit = haystack.Length - len;
+            for (var i = 0; i <= limit; i++)
+            {
+                var k = 0;
+                for (; k < len; k++)
+                {
+                    if (needle[k] != haystack[i + k]) break;
+                }
+                if (k == len) return i;
+            }
+            return -1;
+        }
+
+        public static byte[] ToByteArray(System.IO.Stream stream)
+        {
+            stream.Position = 0;
+            byte[] buffer = new byte[stream.Length];
+            for (int totalBytesCopied = 0; totalBytesCopied < stream.Length;)
+                totalBytesCopied += stream.Read(buffer, totalBytesCopied, Convert.ToInt32(stream.Length) - totalBytesCopied);
+            return buffer;
+        }
+    }
+}
